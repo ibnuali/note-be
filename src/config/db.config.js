b5e943689e6d8fd54d586d23aa0ad7e49c7e106f
Ibnu Ali <ibnuali6@gmail.com>
2023-08-22 07:11:42 +0700

    update config db

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,18 +1,24 @@
-const { Sequelize } = require('sequelize');
-const dotenv = require('dotenv');
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+const pg = require("pg");
+dotenv.config({ path: ".env" });
 
-dotenv.config({ path: '.env' });
-
-const db = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+const db = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
     host: process.env.DATABASE_HOST,
-    dialect: 'postgres',
+    dialect: "postgres",
+    dialectModule: pg,
     logging: false,
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 module.exports = db;
